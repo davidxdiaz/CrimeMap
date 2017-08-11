222a5bf3b5e0c13bea7f2a805a845272b2d479ec
aledavila98 <aled98.ad@gmail.com>
2017-08-10 18:43:47 -0600

    agregado el json de google firebase

--- app/src/main/java/com/example/emprendimiento/crimemap/Delito.java
+++ app/src/main/java/com/example/emprendimiento/crimemap/Delito.java
@@ -1,9 +1,81 @@
 package com.example.emprendimiento.crimemap;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.*;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.Marker;
+
 /**
  * Created by David on 10/8/2017.
  */
 
-public class Delito {
+public abstract class Delito extends Context {
+
+    String comentario;
+    LocationManager locationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS, latitudeGPS;
+    double longitudeNetwork, latitudeNetwork;
+    Marker ubicacion;
+
+    Delito(String comment){
+        this.comentario = comment;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        generarMarcador();
+    }
+    //funcion que genera un marcador en el mapa.
+    //se emplea en las clases hijas.
+    abstract Marker generarMarcador();
+
+    private boolean checkLocation() {
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||
+                !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            showAlert();
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Su ubicación esta desactivada.\npor favor active su ubicación " +
+                        "usa esta app")
+                .setPositiveButton("Configuración de ubicación", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+        @Override
+        public void onProviderEnabled(String s) {}
+
+        @Override
+        public void onProviderDisabled(String s) {}
+    };
 
 }
